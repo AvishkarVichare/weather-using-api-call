217f09df9dde368d52c836edccd43546d4f9940d
AvishkarVichare <avie9500@gmail.com>
2021-11-28 13:39:41 +0530

    new commit

--- app.js
+++ app.js
@@ -3,16 +3,32 @@ const ejs = require('ejs')
 const app = express();
 const path = require("path")
 const requests =require('requests')
-
-
-
+var bodyParser = require('body-parser');
 app.set('view engine','ejs')
 app.set('views',path.join(__dirname,'views'))
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use('/static',express.static('static'));
 
 
-  cityName = 'Mumbai'
+
+
 app.get('/',(req,res)=>{
-requests('https://api.openweathermap.org/data/2.5/weather?q=Mumbai&appid=d396d8030cc2be51bfd84bb05272ef59')
+
+
+  res.render('index.ejs')
+
+})
+
+
+
+
+ 
+app.get('/weather',(req,res)=>{
+ let cityName = req.query.cityName;
+requests(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=d396d8030cc2be51bfd84bb05272ef59`)
 
 
 .on('data',(chunk)=>{
@@ -22,7 +38,9 @@ requests('https://api.openweathermap.org/data/2.5/weather?q=Mumbai&appid=d396d80
  temperatureMin=Math.floor((arrData[0].main.temp_min)-273.15)
  temperatureMax=Math.floor((arrData[0].main.temp_max)-273.15)
  weather = arrData[0].weather[0].main
-  console.log(arrData[0])
+ country = arrData[0].sys.country
+ city=cityName;
+  console.log(arrData)
   res.render('weather.ejs')
 })
 
